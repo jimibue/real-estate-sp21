b4269ea7ad4e48cdbdb16cccd28865a38398433b
James Yeates <jimibue@gmail.com>
2021-03-30 12:31:35 -0600

    getting data

--- client/src/pages/Available.js
+++ client/src/pages/Available.js
@@ -1,12 +1,30 @@
-import {useState, UseEffect, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Available =(props)=>{
-    useEffect(()=>{},[])
+    const  [properties, setProperties] = useState(null)
+
+    useEffect( ()=>{
+        getData()
+    },[])
+
+    const getData = async()=>{
+        try{
+        let res = await axios.get('/api/properties')
+
+        setProperties(res.data)
+        }catch(err){
+            alert('err')
+            console.log(err)
+        }
+    }
+
+    if(!properties) return <p>Loading</p>
 
     return (
         <div>
             <h1>Available</h1>
+            <pre>{JSON.stringify(properties,null,2)}</pre>
         </div>
     )
 }
