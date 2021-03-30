ab37ef39ec9052e08d0d6a48449df0da4ffa416a
James Yeates <jimibue@gmail.com>
2021-03-30 12:25:56 -0600

    nav setup

---
+++ client/src/pages/Available.js
@@ -0,0 +1,14 @@
+import {useState, UseEffect, useEffect} from 'react'
+import axios from 'axios'
+
+const Available =(props)=>{
+    useEffect(()=>{},[])
+
+    return (
+        <div>
+            <h1>Available</h1>
+        </div>
+    )
+}
+
+export default Available
